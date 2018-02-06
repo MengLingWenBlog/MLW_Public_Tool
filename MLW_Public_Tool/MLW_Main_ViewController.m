c90a0993325f4241baf6bbb5e36e0cd4e626ea05
MengLingWenBlog <MengLingWenBlog@github.com>
2018-02-06 16:44:22 +0800

    Test_Commit_Xcode

--- MLW_Public_Tool/MLW_Main_ViewController.m
+++ MLW_Public_Tool/MLW_Main_ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.title = @"主页";
+    //修改一下下
 }
 
 - (void)didReceiveMemoryWarning {
